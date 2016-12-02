7918af3bdfe9dc69c1c8ccf3b8ed0ef5a6b6c2c3
zhangshoudi <zhangshoudi@didichuxing.com>
2016-12-02 14:04:02 +0800

    debug

--- test/test01.py
+++ test/test01.py
@@ -5,15 +5,12 @@ import sys
 import os
 from pyspark import SparkContext, SparkConf
 
-print os.environ['SPARK_HOME'], "heihei"
-print os.environ['PYTHONPATH'], "heihei"
-
 logFile = "test.iml"
 print "spark 运行模式:", sys.argv[1]
 sc = SparkContext(sys.argv[1], 'simple demo')
 sc.setLogLevel('ERROR')
 
-logData = sc.textFile(logFile).cache()
+logData = sc.textFile("/user/zhangshoudi/" + logFile).cache()
 
 numAs = logData.filter(lambda s: 'a' in s).count()
 numBs = logData.filter(lambda s: 'b' in s).count()

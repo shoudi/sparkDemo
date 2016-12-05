fc87758a7b14da17ab0f49fa127735b71b693167
zhangshoudi <zhangshoudi@didichuxing.com>
2016-12-05 12:31:25 +0800

    debug

--- test/test02.py
+++ test/test02.py
@@ -6,17 +6,20 @@ import os
 from pyspark import SparkContext, SparkConf
 
 print os.getenv('HADOOP_HOME')
+conf = SparkConf.set("io.compression.codecs", "org.apache.hadoop.io.compress.Lz4Codec")
+
 logFile = "application_1478165914493_2491600.lz4"
 if len(sys.argv) > 1:
     print "spark 运行模式:", sys.argv[1]
-    sc = SparkContext(sys.argv[1], 'simple demo')
+    sc = SparkContext(sys.argv[1], 'simple demo', conf=conf)
 else:
-    sc = SparkContext(appName='simple demo1')
+    sc = SparkContext(appName='simple demo1', conf=conf)
+
 sc.setLogLevel('ERROR')
 
 # logData = sc.textFile("/user/zhangshoudi/" + logFile).cache()
 logData = sc.textFile("" + logFile)
 counts = logData.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
 
-counts.saveAsTextFile("/user/zhangshoudi/20161205")
-counts.foreach.println
+#counts.saveAsTextFile("/user/zhangshoudi/20161205")
+counts.saveAsTextFile('/user/zhangshoudi/20161205/02', 'LZ4CompressionCodec')
